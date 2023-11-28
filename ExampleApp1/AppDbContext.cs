e8d5643fe8887aac88e6bd9f6144df3363ae52f4
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-11-29 00:57:53 +0600

    example application added

---
+++ ExampleApp1/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExampleApp1;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+}
