75932647df4498ffa92147505487cd9a9063806c
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-11-28 23:59:12 +0600

    add register generic layer static extension methods

---
+++ DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sayeed.Generic.OnionArchitecture.Controller;
+using Sayeed.Generic.OnionArchitecture.Logic;
+using Sayeed.Generic.OnionArchitecture.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayeed.Generic.OnionArchitecture;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterGenericControllerLayer(this IServiceCollection services)
+    {
+        return services.AddScoped(typeof(IBaseController<>), typeof(BaseController<>));
+    }
+
+    public static IServiceCollection RegisterGenericLogicLayer(this IServiceCollection services)
+    {
+        return services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+
+    }
+
+    public static IServiceCollection RegisterGenericRepositoryLayer(this IServiceCollection services)
+    {
+        return services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+    }
+}
