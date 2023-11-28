b84825c9853c878b70f50a812505bda6afc17baf
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-11-28 23:50:34 +0600

    add base entity and enum

---
+++ Entity/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayeed.Generic.OnionArchitecture.Entity;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        Id = 0;
+        CreatedDateUtc = DateTime.UtcNow;
+        UpdatedDateUtc = DateTime.UtcNow;
+        DeletedDateUtc = null;
+        Status = Status.Active;
+    }
+
+    public long Id { get; protected set; }
+    public DateTime CreatedDateUtc { get; protected set; }
+    public DateTime? UpdatedDateUtc { get; protected set; }
+    public DateTime? DeletedDateUtc { get; protected set; }
+    public Status Status { get; protected set; }
+}
